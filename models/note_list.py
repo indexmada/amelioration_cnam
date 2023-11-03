333222fcf39641352565d830b2f9154ffcfb1267
Mizara983 <pounasatu@gmail.com>
2023-11-03 15:07:35 +0300

    session_name

--- models/note_list.py
+++ models/note_list.py
@@ -7,9 +7,15 @@ class NoteList(models.Model):
 
 	code_ue_stored = fields.Char(string="Code UE", compute="compute_code_ue_intitule_ue", store=True)
 	intitule_ue_stored = fields.Char(string="Intitulé UE", compute="compute_code_ue_intitule_ue", store=True)
+	session_stored = fields.Char(string="Session", compute="compute_session_value", store=True)
 
 	@api.depends('unit_enseigne')
 	def compute_code_ue_intitule_ue(self):
 		for record in self:
 			record.code_ue_stored = record.unit_enseigne.code
 			record.intitule_ue_stored = record.unit_enseigne.name
+
+	@api.depends("note_list_filter_id", "note_list_filter_id.session", "note_list_filter_id.session.name")
+	def compute_session_value(self):
+		for record in self:
+			record.session_stored = record.note_list_filter_id.session.name
