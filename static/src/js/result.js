51c12a84b35583055d55ed8363d4d5ad73c52f50
Mizara983 <pounasatu@gmail.com>
2023-11-14 13:17:57 +0300

    iofgdf

--- static/src/js/result.js
+++ static/src/js/result.js
@@ -3,6 +3,6 @@ function year_clicked(year_id) {
 }
 
 function show_result(year_id, session, ue_id) {
-    $("#result").show();
+    $("#result").css("display", "flex");
     $("#result_content").load('/show_result_content/'+year_id+'/'+session+'/'+ue_id);
 }
