ff785856341be8c2be1abd2259fbfc68f85359b7
Mizara983 <pounasatu@gmail.com>
2023-11-14 12:31:22 +0300

    eort

--- static/src/js/result.js
+++ static/src/js/result.js
@@ -1,3 +1,7 @@
 function year_clicked(year_id) {
     $("#ue_section").load('/load_ue_section_by_year/'+year_id)
 }
+
+function show_result(year_id, session, ue_id) {
+    console.log(year_id, session, ue_id)
+}
