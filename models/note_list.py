0c981796b195a3f9a4022cf4e5204d3cabca1496
Mizara983 <pounasatu@gmail.com>
2023-11-14 11:08:24 +0300

    frg

--- models/note_list.py
+++ models/note_list.py
@@ -24,5 +24,5 @@ class NoteListFilter(models.Model):
 	_inherit = "note.list.filter"
 
 	def get_ue_ids(self, year, session = 1):
-		result = self.sudo().search([('year', '=', year)]).filtered(lambda x: x.session.name.find(str(session)) >= 0).mapped('unit.enseigne')
+		result = self.sudo().search([('year', '=', year)]).filtered(lambda x: x.session.name.find(str(session)) >= 0).mapped('unit_enseigne')
 		return result
