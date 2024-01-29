436b09eeae9524ef0731e70cd9e34bed5f4ec32c
Mizara983 <pounasatu@gmail.com>
2024-01-29 15:50:11 +0300

    test resultat

--- static/src/js/result.js
+++ static/src/js/result.js
@@ -1,8 +1,10 @@
 function year_clicked(year_id) {
+    console.log("year_clicked: "+year_id)
     $("#ue_section").load('/load_ue_section_by_year/'+year_id+'/0');
 }
 
 function show_result(year_id, session, ue_id) {
+    console.log("show_result: "+year_id+', '+session+', ', ue_id)
     $("#result").css("display", "flex");
     $("#result_content").load('/show_result_content/'+year_id+'/'+session+'/'+ue_id+'/0');
 }
